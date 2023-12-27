15f870dafa8e6bd14dd343a45e498b195891add4
Denys Halachian <dhalachian@bamboopayment.com>
2023-12-26 23:05:40 -0300

    Ecnrypt BlackBox.

---
+++ src/SensitiveData.CTF.BlackBox/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SensitiveData.CTF.BlackBox
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length != 2)
+            {
+                Console.WriteLine("-url [url]");
+                Environment.Exit(0);
+            }
+            string body = EncryptString(CreateRequest(), "75b7391b2cbade4fe8bbb1e292167db5");
+            HttpClient client = new HttpClient();
+            client.PostAsync(args[1], new StringContent(body, Encoding.UTF8, "application/json")).GetAwaiter().GetResult();
+        }
+
+        public static string EncryptString(string text, string keyString)
+        {
+            byte[] key = Encoding.UTF8.GetBytes(keyString);
+            using (Aes aesAlg = Aes.Create())
+            {
+                using (var encryptor = aesAlg.CreateEncryptor(key, aesAlg.IV))
+                {
+                    using (var msEncrypt = new MemoryStream())
+                    {
+                        using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                        using (var swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(text);
+                        }
+
+                        byte[] iv = aesAlg.IV;
+                        byte[] decryptedContent = msEncrypt.ToArray();
+                        byte[] result = new byte[iv.Length + decryptedContent.Length];
+
+                        Buffer.BlockCopy(iv, 0, result, 0, iv.Length);
+                        Buffer.BlockCopy(decryptedContent, 0, result, iv.Length, decryptedContent.Length);
+
+                        return Convert.ToBase64String(result);
+                    }
+                }
+            }
+        }
+
+        public static string  CreateRequest()
+        {
+            return System.Text.Json.JsonSerializer.Serialize(new 
+            {
+                email = new 
+                {
+                  value = "john.doe@bamboopayment.com" 
+                },
+                pan = new
+                {
+                  value = "4456530000100001"
+                },
+                cvv = new 
+                {
+                  value = "512"
+                },
+                expiration = new 
+                {
+                  value = "10/25"
+                },
+                cardOwner = new
+                {
+                  name = "John Doe"
+                }
+            });
+        }
+    }
+}
---
+++ src/SensitiveData.CTF.TokenizerAPI/Middleware/DecryptBodyMiddleware.cs
@@ -0,0 +1,70 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace SensitiveData.CTF.TokenizerAPI.Middleware
+{
+    public class DecryptBodyMiddleware
+    {
+
+        private readonly RequestDelegate _next;
+        public DecryptBodyMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Stream originalBody = context.Response.Body;
+            try
+            {
+                using (var memStream = new MemoryStream())
+                {
+                    context.Response.Body = memStream;
+                    await _next(context);
+                    memStream.Position = 0;
+                    memStream.Position = 0;
+                    await memStream.CopyToAsync(originalBody);
+                }
+            }
+            finally {
+                context.Response.Body = originalBody;
+            }
+        }
+        private async Task<string> GetBodyAsync(HttpContext context)
+        {
+            context.Request.EnableBuffering();
+            using var reader = new StreamReader(
+                context.Request.Body,
+                Encoding.UTF8,
+                detectEncodingFromByteOrderMarks: false,
+                leaveOpen: true);
+            return await reader.ReadToEndAsync();
+        }
+        public static string DecryptString(string cipherText, string keyString = "75b7391b2cbade4fe8bbb1e292167db5")
+        {
+            byte[] fullCipher = Convert.FromBase64String(cipherText);
+
+            byte[] iv = new byte[16];
+            byte[] cipher = new byte[fullCipher.Length - iv.Length];
+
+            Buffer.BlockCopy(fullCipher, 0, iv, 0, iv.Length);
+            Buffer.BlockCopy(fullCipher, iv.Length, cipher, 0, cipher.Length);
+
+            byte[] key = Encoding.UTF8.GetBytes(keyString);
+
+            using (Aes aesAlg = Aes.Create())
+            {
+                using (var decryptor = aesAlg.CreateDecryptor(key, iv))
+                {
+                    using (var msDecrypt = new MemoryStream(cipher))
+                    {
+                        using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                        using (var srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            return srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }    
+    }
+}
